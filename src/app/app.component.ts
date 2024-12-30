394f56b25cca96eae83d3785f81ec25188e6369a
joycepwong <joycepwong@yahoo.ca>
2024-12-29 21:52:40 -0500

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [ CommonModule, FormsModule ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'busyqa-11-angular-4-coursework-initial';
+  title = 'To Do List';
+
+  newTask: string = "";
+  tasks: string[] = [];
+  mode: string = "";
+
+  onAddTask(): void {
+    this.tasks.push(this.newTask);
+    this.newTask = "";
+    this.mode = "added";
+    setTimeout(() => {
+      this.mode = '';
+    }, 5000);
+  }
+
+  onRemove(index: number): void {
+    this.tasks.splice(index, 1);
+    this.mode = "removed";
+    setTimeout(() => {
+      this.mode = '';
+    }, 5000);
+  }
 }
